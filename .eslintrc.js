b44441a50ab62a1a9ae4e760aba22cb1e01caeee
Julien Usson <usson.julien@gmail.com>
2018-07-26 11:28:01 +0200

    :wrench: code quality

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  parser: "babel-eslint",
+  extends: [
+    "eslint:recommended",
+    "plugin:jest/recommended",
+    "plugin:react/recommended",
+    "prettier",
+    "prettier/react"
+  ],
+  plugins: ["jest", "react", "prettier"],
+  env: {
+    "jest/globals": true,
+    browser: true,
+    node: true,
+    es6: true
+  },
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        semi: false,
+        singleQuote: true,
+        trailingComma: "es5"
+      }
+    ]
+  }
+};
