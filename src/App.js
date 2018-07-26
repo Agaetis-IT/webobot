0752a27fb7498d132190696d20b36124dc257276
Julien Usson <usson.julien@gmail.com>
2018-07-26 11:42:55 +0200

    :sparkles: material ui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { CssBaseline, MuiThemeProvider } from '@material-ui/core'
 import Routes from './Routes'
+import theme from './theme'
 
-const App = () => <Routes />
+const App = () => (
+  <Fragment>
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      <Routes />
+    </MuiThemeProvider>
+  </Fragment>
+)
 
 export default App
