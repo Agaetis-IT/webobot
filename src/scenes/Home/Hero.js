53592480d442b765fb1ae1fa0e099d69a53aea90
Julien Usson <usson.julien@gmail.com>
2018-08-01 16:58:30 +0200

    :sparkles: hero content

--- src/scenes/Home/Hero.js
+++ src/scenes/Home/Hero.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import propTypes from 'prop-types'
-import { withStyles } from '@material-ui/core'
+import { Button, Typography, withStyles } from '@material-ui/core'
 import hero from 'images/hero.png'
 import DownArrow from './DownArrow'
 import { compose } from 'recompose'
 import withScrollManager from 'enhancers/withScrollManager'
 
-const styles = {
+const styles = theme => ({
   hero: {
     height: '100vh',
     minHeight: 300,
@@ -15,12 +15,21 @@ const styles = {
     backgroundRepeat: 'no-repeat',
     backgroundSize: 'cover',
     position: 'relative',
+    display: 'flex',
+    alignItems: 'center',
   },
   heroImg: {
     width: 'auto',
     height: '100%',
   },
-}
+  container: {
+    marginLeft: '10%',
+  },
+  txt: {
+    textShadow: theme.shadows[24],
+    marginBottom: 20,
+  },
+})
 
 class Hero extends Component {
   constructor(props) {
@@ -37,6 +46,25 @@ class Hero extends Component {
     const { classes } = this.props
     return (
       <div className={classes.hero} ref={this.heroRef}>
+        <div className={classes.container}>
+          <Typography
+            variant="display2"
+            color="primary"
+            className={classes.txt}
+          >
+            THE<br />MONOBLOC<br />PROJECT
+          </Typography>
+          <Typography
+            variant="headline"
+            color="primary"
+            className={classes.txt}
+          >
+            Apportez votre bloc à l’édifice !
+          </Typography>
+          <Button variant="contained" color="secondary">
+            DÉMARRER
+          </Button>
+        </div>
         <DownArrow onClick={this.onDownArrowClick} />
       </div>
     )
