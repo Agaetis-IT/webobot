7b3a58564b13c77e389557c154324d33160e9b21
Julien Usson <usson.julien@gmail.com>
2018-08-01 16:39:01 +0200

    :sparkles: responsive menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import propTypes from 'prop-types'
 import {
   AppBar,
   Button,
+  Drawer,
   Icon,
   IconButton,
+  List,
+  ListItem,
+  ListItemText,
   Hidden,
   Toolbar,
   Typography,
@@ -18,30 +22,77 @@ const styles = {
     justifyContent: 'space-between',
     alignItems: 'center',
   },
+  list: {
+    width: 300,
+  },
 }
 
-const Header = ({ classes }) => (
-  <AppBar position="absolute" color="primary">
-    <Toolbar className={classes.toolbar}>
-      <Link to="/">
-        <Typography variant="title">The monobloc project</Typography>
-      </Link>
-      <Hidden smDown implementation="css">
-        <Link to="/">
-          <Button>Le projet</Button>
-        </Link>
-        <Link to="/">
-          <Button>Contribuer</Button>
-        </Link>
-      </Hidden>
-      <Hidden mdUp implementation="css">
-        <IconButton>
-          <Icon>menu</Icon>
-        </IconButton>
-      </Hidden>
-    </Toolbar>
-  </AppBar>
-)
+const menu = [
+  { name: 'Le projet', href: '/' },
+  { name: 'Contribuer', href: '/' },
+]
+
+class Header extends React.Component {
+  state = {
+    open: false,
+  }
+
+  toggleDrawer = open => () => {
+    this.setState({
+      open,
+    })
+  }
+
+  render() {
+    const { classes } = this.props
+    const { open } = this.state
+
+    return (
+      <Fragment>
+        <AppBar position="absolute" color="primary">
+          <Toolbar className={classes.toolbar}>
+            <Link to="/">
+              <Typography variant="title">The monobloc project</Typography>
+            </Link>
+            <Hidden smDown implementation="css">
+              {menu.map(({ name, href }) => (
+                <Link key={name} to={href}>
+                  <Button>{name}t</Button>
+                </Link>
+              ))}
+            </Hidden>
+            <Hidden mdUp implementation="css">
+              <IconButton onClick={this.toggleDrawer(true)}>
+                <Icon>menu</Icon>
+              </IconButton>
+            </Hidden>
+          </Toolbar>
+        </AppBar>
+
+        <Drawer open={open} onClose={this.toggleDrawer(false)}>
+          <div
+            tabIndex={0}
+            role="button"
+            onClick={this.toggleDrawer(false)}
+            onKeyDown={this.toggleDrawer(false)}
+          >
+            <div className={classes.list}>
+              <List>
+                {menu.map(({ name, href }) => (
+                  <Link key={name} to={href}>
+                    <ListItem button>
+                      <ListItemText primary={name} />
+                    </ListItem>
+                  </Link>
+                ))}
+              </List>
+            </div>
+          </div>
+        </Drawer>
+      </Fragment>
+    )
+  }
+}
 
 Header.propTypes = {
   classes: propTypes.object.isRequired,
