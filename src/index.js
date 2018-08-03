ac535d8a0c6bbc46a9cf32edbcb2b323f9000563
Julien Usson <usson.julien@gmail.com>
2018-08-03 17:40:26 +0200

    :wrench: hot reload

--- src/index.js
+++ src/index.js
@@ -4,4 +4,12 @@ import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'))
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default
+    ReactDOM.render(<NextApp />, document.getElementById('root'))
+  })
+}
+
 registerServiceWorker()
