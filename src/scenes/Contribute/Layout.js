1b9a18ede5c695da422f7ae271f86c2768f76614
Julien Usson <usson.julien@gmail.com>
2018-08-09 10:41:11 +0200

    :lipstick: responsive layout

--- src/scenes/Contribute/Layout.js
+++ src/scenes/Contribute/Layout.js
@@ -3,10 +3,14 @@ import propTypes from 'prop-types'
 import { withStyles } from '@material-ui/core'
 import Picture from 'components/Picture'
 
-const styles = {
+const styles = theme => ({
   root: {
     display: 'flex',
     justifyContent: 'space-between',
+    flexDirection: 'row',
+    [theme.breakpoints.down('sm')]: {
+      flexDirection: 'column',
+    },
   },
   form: {
     display: 'flex',
@@ -14,11 +18,21 @@ const styles = {
     alignItems: 'center',
     justifyContent: 'space-around',
     width: '50%',
+    paddingLeft: 15,
+    paddingRight: 15,
+    [theme.breakpoints.down('sm')]: {
+      width: '100%',
+      paddingTop: 30,
+      minHeight: 300,
+    },
   },
   picture: {
     width: '50%',
+    [theme.breakpoints.down('sm')]: {
+      width: '100%',
+    },
   },
-}
+})
 
 class Layout extends React.Component {
   render() {
