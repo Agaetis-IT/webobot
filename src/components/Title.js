5e622360255e39931639167caf388d07a8fd689a
Julien Usson <usson.julien@gmail.com>
2018-08-09 12:03:53 +0200

    :sparkles: thanks page

--- src/components/Title.js
+++ src/components/Title.js
@@ -2,15 +2,16 @@ import React from 'react'
 import propTypes from 'prop-types'
 import { Typography, withStyles } from '@material-ui/core'
 import Highlight from './Highlight'
+import classNames from 'classnames'
 
-const styles = theme => ({
+const styles = {
   title: {
     marginBottom: 20,
   },
-})
+}
 
-const Title = ({ classes, children }) => (
-  <Typography variant="title" className={classes.title}>
+const Title = ({ classes, children, className }) => (
+  <Typography variant="title" className={classNames(classes.title, className)}>
     <Highlight>{children}</Highlight>
   </Typography>
 )
@@ -18,6 +19,7 @@ const Title = ({ classes, children }) => (
 Title.propTypes = {
   classes: propTypes.object.isRequired,
   children: propTypes.string.isRequired,
+  className: propTypes.string,
 }
 
 export default withStyles(styles)(Title)
--- src/scenes/Contribute/Thanks.js
+++ src/scenes/Contribute/Thanks.js
@@ -3,8 +3,36 @@ import propTypes from 'prop-types'
 import { Button, Typography, withStyles } from '@material-ui/core'
 import Title from 'components/Title'
 import { Link } from 'react-router-dom'
+import { Twemoji } from 'react-emoji-render'
 
-const styles = {}
+const styles = {
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100%',
+  },
+  clap: {
+    fontSize: 64,
+    marginBottom: 10,
+  },
+  title: {
+    fontSize: 42,
+    fontWeight: 700,
+  },
+  actions: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 30,
+    '& > *': {
+      margin: 5,
+    },
+  },
+}
 
 class Thanks extends React.Component {
   render() {
@@ -12,20 +40,22 @@ class Thanks extends React.Component {
 
     return (
       <div className={classes.root}>
-        <Title>Merci</Title>
+        <Twemoji text=":clap:" className={classes.clap} />
+        <Title className={classes.title}>Merci</Title>
 
         <Typography>
           Grâce à vous, notre intelligence artificielle s’améliore !
         </Typography>
-
-        <Button color="secondary" variant="contained" onClick={onReset}>
-          ANALYSER UNE AUTRE PHOTO
-        </Button>
-        <Link to="/">
-          <Button color="primary" variant="contained">
-            ÇA SUFFIT POUR AUJOURD&apos;HUI
+        <div className={classes.actions}>
+          <Button color="secondary" variant="contained" onClick={onReset}>
+            ANALYSER UNE AUTRE PHOTO
           </Button>
-        </Link>
+          <Link to="/">
+            <Button color="primary" variant="contained">
+              ÇA SUFFIT POUR AUJOURD&apos;HUI
+            </Button>
+          </Link>
+        </div>
       </div>
     )
   }
